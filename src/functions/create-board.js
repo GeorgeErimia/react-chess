493c3dfc75a47fcb3d588cbe5e907916fff1913c
George Erimia <l3ttuceftw@gmail.com>
2025-04-10 11:35:24 +0300

    Finish createBoard method in create-board.js | Add Board component

--- src/functions/create-board.js
+++ src/functions/create-board.js
@@ -14,6 +14,68 @@ const range = (n) => {
     return Array.from({length: n}, (_, i) => i + 1);
 }
 
-export const createboard = () => {
+
+/**
+ * Function that generates the actual chessboard information, as an array of Cell objects
+ * @param {String} fenString rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPPP/RNBQKBNR w KQkq - 0 1
+ * @returns {Cell[]}
+ */
+
+export const createboard = (fenString) => {
+
+    // Get the first portion from the fen (before the first space)
+    const fen = fenString.split(' ')[0];
+
+    // Remove the row delimiters '/'
+    // Result should look like this : rnbqkbnrpppppppp8888PPPPPPPPRNBQKBNR
+    const fenPieces = fenString.split('/').join('');
+
+    // Convert the FEN into a 64-piece array
+    let pieces = Array.from(fenPieces);
+    Array.from(fenPieces).forEach((item, index) => {
+        // Check if there's a number n (of empty spaces) in the FEN
+        if(isFinite(item)) {
+            // Replace the number with n empty spaces 
+            pieces.splice(index, 1, range(item).fill(''));
+        }
+    });
+
+    // Flatten the array
+    pieces = pieces.flat();
+
+
+    // Create rows descending from top to bottom
+    // Should look like this : ["8", "7", "6", "5", "4", "3", "2", "1"]
+    const rows = range(8)
+        .map((n) => n.toString())
+        .reverse();
+
+    // Create columns
+    const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    // Create an array of cells from the rows and columns 
+    // Should look like this : ["a1", "b1", "c1", ...]
+    const cells = [];
+    for(let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        for(let j = 0; j < columns.length; j++) {
+            const col = columns[i];
+            cells.push(col + row);
+            // NOTE: The cells array will start from the top left (a8) and end on the bottom right (h1);
+        }
+    }
     
+    // Create Cell objects for each cell and piece
+    // Create a board of Cell objects
+    const board = [];
+    for(let i = 0; i < cells.length; i++) {
+        // 'cells' and 'pieces' have the length of 64
+        const cell = cells[i];
+        const piece = pieces[i];
+        board.push(new Cell(cell, piece));
+    }
+
+    console.log(board);
+    return board;
 }
+
--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Chess } from 'chess.js';
+import { createboard } from '../../functions/create-board';
+import Board from '../../components/board';
 
 // Define a FEN
 // A FEN is a string notation used to describe a particular board position of a chess game
@@ -13,8 +15,15 @@ const FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
 const Game = () => {
     const [fen, setFen] = useState(FEN);
     const {current: chess} = useRef(new Chess(fen));
+    const [board, setBoard] = useState(createboard(fen));
 
-    return <div></div>
+    useEffect(() => {
+        setBoard(createboard(fen));
+    }, [fen]);
+
+    return <div className='game'>
+        <Board cells={board} />
+    </div>
 };
 
 export default Game;
