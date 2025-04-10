2a54d80a6787236dc3c0e47637d8a4ec1831c6ca
George Erimia <l3ttuceftw@gmail.com>
2025-04-10 10:12:43 +0300

    Add game component and initialize FEN

---
+++ src/pages/Game/index.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from 'react';
+import { Chess } from 'chess.js';
+
+// Define a FEN
+// A FEN is a string notation used to describe a particular board position of a chess game
+// This FEN represents the initial positions on the chess board
+// Uppercase - white pieces | Lowercase - black pieces
+const FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+const Game = () => {
+    const [fen, setFen] = useState(FEN);
+    const {current: chess} = useRef(new Chess(fen));
+
+    return <div></div>
+};
+
+export default Game;
